fedf92a82c42f59b15ce957c7feb6c3399409093
kumuditha-udayanga <kumudithaudaya@gmail.com>
2025-07-17 21:12:57 +0530

    Define user controlers

---
+++ Backend/src/controllers/userController.js
@@ -0,0 +1,15 @@
+import User from '../models/user.js';
+
+export const getUserProfile = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.json({ user });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch profile' });
+    }
+};
